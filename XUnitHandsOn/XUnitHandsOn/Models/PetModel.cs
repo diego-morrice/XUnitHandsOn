6edaa1f48c2b2f71a472a17dc36d73ec04e5ec02
Diego Morrice <diego.morrice@outlook.com>
2018-06-17 00:40:18 -0300

    missing parts

---
+++ XUnitHandsOn/XUnitHandsOn/Models/PetModel.cs
@@ -0,0 +1,29 @@
+﻿using XUnitHandsOn.Querys;
+
+namespace XUnitHandsOn.Models
+{
+    public class PetModel
+    {
+        public enum PetGender
+        {
+            Macho,
+            Femea
+        }
+
+        public enum PetType
+        {
+            Cachorro,
+            Gato,
+            Hamster,
+            Peixe,
+            Passarinho,
+            Porco
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public PetType Type { get; set; }
+        public PetGender Gender { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
